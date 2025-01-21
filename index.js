d72708de90258a68d2196ef87d9b687423224cde
Pranav-cse-kalvium <pranav.prakash@kalvium.community>
2025-01-21 14:29:39 +0530

    api_assign_02

--- index.js
+++ index.js
@@ -66,5 +66,5 @@ app.delete('/books/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
